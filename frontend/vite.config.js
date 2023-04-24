7967dabbeefd246bd5ae430e4e835279d8b8eb38
Nitin Yadav <nitin.yadav.ny237@gmail.com>
2023-04-24 21:54:42 +0530

    vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,24 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://ny-techno-travel.vercel.app',
+        changeOrigin: true,
+        secure: false,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, PATCH, OPTIONS',
+          'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+          'Access-Control-Allow-Credentials': 'true'
+        }
+      }
+    }
+  }
 })
