3ff2c172751cc7fb15337f037ed985237151e6c0
Nitin Yadav <nitin.yadav.ny237@gmail.com>
2023-04-25 10:29:05 +0530

    vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,47 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
   server: {
+    cors: true,
     proxy: {
-      '/api/v1': {
+      '/api/v1/auth/register': {
         target: 'https://ny-techno-travel.vercel.app',
         changeOrigin: true,
-        secure: false,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, PATCH, OPTIONS',
+          'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+          'Access-Control-Allow-Credentials': 'true'
+        }
+      },
+      '/api/v1/auth/login': {
+        target: 'https://ny-techno-travel.vercel.app',
+        changeOrigin: true,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, PATCH, OPTIONS',
+          'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+          'Access-Control-Allow-Credentials': 'true'
+        }
+      },
+      '/api/v1/tours/*': {
+        target: 'https://ny-techno-travel.vercel.app',
+        changeOrigin: true,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, PATCH, OPTIONS',
+          'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+          'Access-Control-Allow-Credentials': 'true'
+        }
+      },
+      '/api/v1/reviews/*': {
+        target: 'https://ny-techno-travel.vercel.app',
+        changeOrigin: true,
         headers: {
           'Access-Control-Allow-Origin': '*',
           'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, PATCH, OPTIONS',
@@ -22,4 +51,4 @@ export default defineConfig({
       }
     }
   }
-})
+});
