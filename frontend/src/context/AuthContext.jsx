36cf757c69e63e338cdc9ab040065ee2fbd9a2c4
Nitin Yadav <nitin.yadav.ny237@gmail.com>
2023-04-26 21:55:59 +0530

    vercel

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -3,60 +3,72 @@ import { createContext } from "react";
 
 const initial_state = {
   user:
-    localStorage.getItem("user") != undefined
+    localStorage.getItem("user") !== undefined && localStorage.getItem("user") !== null
       ? JSON.parse(localStorage.getItem("user"))
       : null,
   loading: false,
   error: null,
+  token:
+    localStorage.getItem("token") !== undefined && localStorage.getItem("token") !== null
+      ? localStorage.getItem("token")
+      : null,
 };
 
 export const AuthContext = createContext(initial_state);
+
 const AuthReducer = (state, action) => {
   switch (action.type) {
     case "LOGIN_START":
       return {
+        ...state,
         user: null,
         loading: true,
         error: null,
       };
     case "LOGIN_SUCCESS":
       return {
-        user: action.payload,
+        ...state,
+        user: action.payload.user,
         loading: false,
         error: null,
+        token: action.payload.token,
       };
     case "LOGIN_FAILURE":
       return {
+        ...state,
         user: null,
         loading: false,
         error: action.payload,
+        token: null,
       };
     case "REGISTER_SUCCESS":
       return {
+        ...state,
         user: null,
         loading: false,
         error: null,
+        token: null,
       };
     case "LOGOUT":
       return {
+        ...state,
         user: null,
         loading: false,
         error: null,
+        token: null,
       };
-
     default:
       return state;
   }
 };
-
-//
 
 export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AuthReducer, initial_state);
 
   useEffect(() => {
     localStorage.setItem("user", JSON.stringify(state.user));
-  }, [state.user]);
+    localStorage.setItem("token", state.token);
+  }, [state.user, state.token]);
 
   return (
     <AuthContext.Provider
@@ -64,6 +76,7 @@ export const AuthContextProvider = ({ children }) => {
         user: state.user,
         loading: state.loading,
         error: state.error,
+        token: state.token,
         dispatch,
       }}
     >
