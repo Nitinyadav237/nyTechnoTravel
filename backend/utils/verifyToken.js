bb9da314f66c9c76ae66e56b0e202664f8d32f66
Nitin Yadav <nitin.yadav.ny237@gmail.com>
2023-04-26 20:36:57 +0530

    vercel

--- backend/utils/verifyToken.js
+++ backend/utils/verifyToken.js
@@ -4,14 +4,14 @@ export const verifyToken = (req, res, next) => {
   const token = req.cookies.accessToken;
   console.log(token)
  
-  // if (!token) {
-  //   console.log(token)
-  //   return res.status(401).json({
-  //     success: false,
-  //     token,
-  //     message: "You're Not Authorize verify token",
-  //   });
-  // }
+  if (!token) {
+    console.log(token)
+    return res.status(401).json({
+      success: false,
+      token,
+      message: "You're Not Authorize verify token",
+    });
+  }
 
   //if token is exist then verify the token
   jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
